2ce6bc3bdb96dbda1d61fe441f3b8ea43b62d050
Zhou <1041341764@qq.com>
2017-05-17 22:12:48 +0800

    2

--- StudentManageSystem/Form1.Designer.cs
+++ StudentManageSystem/Form1.Designer.cs
@@ -35,11 +35,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(278, 9);
+            this.label1.Location = new System.Drawing.Point(374, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(295, 35);
+            this.label1.Size = new System.Drawing.Size(85, 35);
             this.label1.TabIndex = 0;
-            this.label1.Text = "学生信息管理系统";
+            this.label1.Text = "登录";
             // 
             // Form1
             // 
